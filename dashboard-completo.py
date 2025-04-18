5d1cb00bc74eb08145f555d1322674fa195eda44
Dudsclahs <caiolahsilva@gmail.com>
2025-04-18 15:18:40 -0300

    Update dashboard-completo.py

--- dashboard-completo.py
+++ dashboard-completo.py
@@ -110,21 +110,22 @@ if "Ano/Mes" in df_filtrado.columns:
 
 # GRÁFICO 7
 if "Tipo de Frota" in df_filtrado.columns and not df_filtrado["Tipo de Frota"].dropna().empty:
-    tipos_frota = df_filtrado["Tipo de Frota"].value_counts().reset_index()
+    tipos_frota = df_filtrado["Tipo de Frota"].value_counts().reset_index().head(15)
     tipos_frota.columns = ["Tipo de Frota", "Ocorrências"]
     tipos_frota = tipos_frota.sort_values("Ocorrências", ascending=False)
     plot_horizontal_bar(tipos_frota, "Ocorrências", "Tipo de Frota", ["Tipo de Frota", "Ocorrências"], "Tipos de Frota com Mais Ocorrências")
 
 # GRÁFICO 8
 if "Descrição da Frota" in df_filtrado.columns and not df_filtrado["Descrição da Frota"].dropna().empty:
-    descricao_frota = df_filtrado["Descrição da Frota"].value_counts().reset_index()
+    descricao_frota = df_filtrado["Descrição da Frota"].value_counts().reset_index().head(15)
     descricao_frota.columns = ["Descrição da Frota", "Ocorrências"]
-    descricao_frota = descricao_frota.sort_values("Ocorrências", ascending=False).head(20)
+    descricao_frota = descricao_frota.sort_values("Ocorrências", ascending=False)
     plot_horizontal_bar(descricao_frota, "Ocorrências", "Descrição da Frota", ["Descrição da Frota", "Ocorrências"], "Frotas mais Frequentes (Descrição da Frota)")
 
 # GRÁFICO 9
 if "Tipo de Manutenção" in df_filtrado.columns and not df_filtrado["Tipo de Manutenção"].dropna().empty:
-    tipo_manutencao = df_filtrado["Tipo de Manutenção"].value_counts().reset_index()
+    tipo_manutencao = df_filtrado["Tipo de Manutenção"].value_counts().reset_index().head(15)
     tipo_manutencao.columns = ["Tipo de Manutenção", "Ocorrências"]
     tipo_manutencao = tipo_manutencao.sort_values("Ocorrências", ascending=False)
     plot_horizontal_bar(tipo_manutencao, "Ocorrências", "Tipo de Manutenção", ["Tipo de Manutenção", "Ocorrências"], "Distribuição por Tipo de Manutenção")
+
