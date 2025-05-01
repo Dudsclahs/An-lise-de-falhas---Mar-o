dbb53c24ab0f1596b3067e9bef24de2ee683a4df
Dudsclahs <caiolahsilva@gmail.com>
2025-05-01 15:12:41 -0300

    Update dashboard-completo.py

--- dashboard-completo.py
+++ dashboard-completo.py
@@ -138,7 +138,7 @@ st.altair_chart(chart5, use_container_width=True)
 tendencia_entrada = df_filtrado[df_filtrado["Entrada"].notna()].copy()
 tendencia_entrada["Data de Entrada"] = tendencia_entrada["Entrada"].dt.date
 tendencia_entrada = (
-    df_filtrado[df_filtrado["Entrada"].notna()]
+    df_filtrado["Entrada"] = df_filtrado["Entrada"].dt.normalize()
     .groupby("Entrada")["Boletim"]
     .count()
     .reset_index()
@@ -147,7 +147,7 @@ tendencia_entrada.columns = ["Data de Entrada", "Quantidade"]
 
 chart7 = alt.Chart(tendencia_entrada).mark_bar(color="green").encode(
     x=alt.X("Data de Entrada:T", title="Data de Entrada", axis=alt.Axis(format="%d/%m")),
-  y=alt.Y("Quantidade:Q", title="Quantidade de OS", scale=alt.Scale(domainMin=1), axis=alt.Axis(tickMinStep=1)),
+    y=alt.Y("Quantidade:Q", title="Quantidade de OS", scale=alt.Scale(domainMin=1), axis=alt.Axis(tickMinStep=1)),
     tooltip=["Data de Entrada", "Quantidade"]
 ).properties(width=800, height=400)
 
